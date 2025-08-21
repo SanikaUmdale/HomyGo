366d547e332bb6fd72654a780d886710c5799072
Sanika <sanikaumdale@gmail.com>
2025-08-21 12:39:17 +0530

    1st commit

---
+++ routes/listing.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
+// Index Route
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+//New or Create Route
+
+router.get("/new", isLoggedIn, (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//show route
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
+      .populate("owner");
+    if (!listing) {
+      req.flash("error", "Listing you requested for dose not exits!");
+      return res.redirect("/listings");
+    }
+    console.log(listing);
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+//Create Route
+router.post(
+  "/",
+  isLoggedIn,
+  validateListing,
+  wrapAsync(async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    // console.log(req.user);
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success", "New listing created!");
+    res.redirect("/listings");
+
+    // let {title,description,image,price,location ,country}=req.body;
+  })
+);
+
+//Edit route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id.trim());
+    if (!listing) {
+      req.flash("error", "Listing you requested for dose not exits!");
+      return res.redirect("/listings");
+    }
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+//Upadate route
+router.put(
+  "/:id",
+  isLoggedIn,
+  isOwner,
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+//Delete route
+router.delete(
+  "/:id",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success", "Listing Is Deleted!");
+    res.redirect("/listings");
+  })
+);
+
+module.exports = router;
---
+++ routes/review.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require("../models/review.js");
+const{validateReview, isLoggedIn,isReviewAuthor}=require("../middleware.js")
+const Listing = require("../models/listing.js");
+
+
+
+//  reviews
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    newReview.author=req.user._id;
+    // console.log(newReview);
+     
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+    req.flash("success", "New Review Is Created!");
+
+    res.redirect(`/listings/${listing._id}`);
+    // console.log("new review saved");
+    // res.send("New Review saved..!");
+  })
+);
+//Delete route =>reviews
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Review Is Deleted!");
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+module.exports = router;
