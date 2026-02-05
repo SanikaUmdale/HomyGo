ab9fcb0c31abcca31381a6d7b59e55d1480f4455
Sanika <sanikaumdale@gmail.com>
2026-02-05 13:31:58 +0530

    added url

--- init/index.js
+++ init/index.js
@@ -29,17 +29,17 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.error("DB connection error:", err);
-  });
+// main()
+//   .then(() => {
+//     console.log("connected to DB");
+//   })
+//   .catch((err) => {
+//     console.error("DB connection error:", err);
+//   });
 
-async function main() {
-  await mongoose.connect(process.env.ATLASDB_URL);
-}
+// async function main() {
+//   await mongoose.connect(process.env.ATLASDB_URL);
+// }
 
 const initDB = async () => {
   await Listing.deleteMany({});
